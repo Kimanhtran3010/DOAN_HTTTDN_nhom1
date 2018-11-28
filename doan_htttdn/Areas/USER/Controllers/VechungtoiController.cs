eeff1c637da03814f2e77932d930c6db404af431
KIM ANH <tkimanh3010@gmail.com>
2018-11-28 13:00:12 +0700

    Add project files.

---
+++ doan_htttdn/Areas/USER/Controllers/VechungtoiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace doan_htttdn.Areas.USER.Controllers
+{
+    public class VechungtoiController : Controller
+    {
+        // GET: USER/Vechungtoi
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
