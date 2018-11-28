5cb790996f5474d80a1e272cdb903208535e798f
fflinhdanhthue1 <ncduy1997@gmail.com>
2018-11-28 16:32:22 +0700

    Duy moi tao

---
+++ doan_htttdn/Areas/USER/Controllers/tintucController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace doan_htttdn.Areas.USER.Controllers
+{
+    public class tintucController : Controller
+    {
+        // GET: USER/tintuc
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
