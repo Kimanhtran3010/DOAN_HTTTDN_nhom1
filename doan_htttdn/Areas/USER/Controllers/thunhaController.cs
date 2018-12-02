8fdcda68467089e0c698508da94fa064fca53392
Trần Văn Đức <vanductran0812@gmail.com>
2018-12-02 19:17:37 +0700

    thay đổi gì thì viết vào đây để mn biết. Đức thử nha mn.

---
+++ doan_htttdn/Areas/USER/Controllers/thunhaController.cs
@@ -0,0 +1,17 @@
+﻿tusing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace doan_htttdn.Areas.USER.Controllers
+{
+    public class thunhaController : Controller
+    {
+        // GET: USER/thunha
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
