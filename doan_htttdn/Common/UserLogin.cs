d9626f2a28bfa5a2fa175367a2e72806514594e9
Hong <rosexl007@gmail.com>
2018-12-03 17:22:36 +0700

    Xong login Admin

---
+++ doan_htttdn/Common/UserLogin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace doan_htttdn.Common
+{
+    [Serializable]
+    public class UserLogin
+    { 
+        public string IDuser { get; set; }
+    }
+}
---
+++ doan_htttdn/DAO/DAO_Admin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using doan_htttdn.FF;
+
+namespace doan_htttdn.DAO
+{
+    public class DAO_Admin
+    {
+        public QL_SCN db = new QL_SCN();
+        public ADMIN Login(String user , string pass)
+        {
+            return db.ADMINs.Where(x => x.IDAmin == user && x.Password == pass).SingleOrDefault();
+        }
+        public bool Insert(ADMIN aDMIN)
+        {
+            try
+            {
+                db.ADMINs.Add(aDMIN);
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            
+        }
+
+    }
+}
