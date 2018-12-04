60c795d52a772a03d1d50627fb16d55defbc7d15
ttnhan97 <truongtienngochan97@gmail.com>
2018-12-05 01:31:23 +0700

    han moi lam

---
+++ doan_htttdn/Assets/GIAOVIEN/js/JavaScript.js
@@ -0,0 +1,43 @@
+﻿//function test() {
+//    var pw1 = document.getElementById('1');
+//    var pw2 = document.getElementById('2');
+
+    
+//    if (pw1 != pw2) {
+//        alert("\nYou did not enter the same new password twice. Please re-enter your password.");
+//        return false;
+//    }
+//    else return true;
+//}
+function matchpass() {
+    var pw0 = document.getElementById('0');
+    var pw1 = document.getElementById('1');
+    var pw2 = document.getElementById('2');
+    alert("mạhjs");
+    if (pw1 != pw2) {
+        alert("Mật khẩu không khớp!");
+        return false;
+    }
+    else
+        return true;
+    if (pw0 == "") {
+        alert("Bạn chưa nhập mật khẩu!");
+        return false;
+    }
+    else
+        return true;
+     
+    }
+    
+}
+//function kiemtra() {
+//    var pw0 = Document.getElementById('0');
+//    var pw1 = Document.getElementById('1');
+//    var pw2 = Document.getElementById('2');
+//    if (pw0 == null || pw1 == null || pw2 == null) {
+//        alert("Vui lòng nhập đủ thông tin:");
+//        return false;
+//    }
+//    return true;
+//}
+// End -->
